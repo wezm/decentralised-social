0ab917ec6f627a13e286233ce8fd5df9dbd19102
Ilja <domainepublic@spectraltheorem.be>
2020-10-23 20:27:13 +0200

    Return maps in node_info
    It's easiest (and imo most proper) to use tuples {"instance, "reason"} in BE,
    but for FE maps like %{"instance": "instance", "reason", "reason"} are better.
    I changed it so that node_info returns maps now for simple_policy and quarantined instances.

--- test/pleroma/web/node_info_test.exs
+++ test/pleroma/web/node_info_test.exs
@@ -152,6 +152,22 @@ defmodule Pleroma.Web.NodeInfoTest do
            )
   end
 
+  test "it shows quarantined instances data if enabled", %{conn: conn} do
+    clear_config([:mrf, :transparency], true)
+
+    quarantined_instances = [{"example.com", ""}]
+    clear_config([:instance, :quarantined_instances], quarantined_instances)
+
+    expected_config = [%{"instance" => "example.com", "reason" => ""}]
+
+    response =
+      conn
+      |> get("/nodeinfo/2.1.json")
+      |> json_response(:ok)
+
+    assert response["metadata"]["federation"]["quarantined_instances"] == expected_config
+  end
+
   test "it shows MRF transparency data if enabled", %{conn: conn} do
     clear_config([:mrf, :policies], [Pleroma.Web.ActivityPub.MRF.SimplePolicy])
     clear_config([:mrf, :transparency], true)
@@ -159,7 +175,7 @@ defmodule Pleroma.Web.NodeInfoTest do
     simple_config = %{"reject" => [{"example.com", ""}]}
     clear_config(:mrf_simple, simple_config)
 
-    expected_config = %{"reject" => [["example.com", ""]]}
+    expected_config = %{"reject" => [%{"instance" => "example.com", "reason" => ""}]}
 
     response =
       conn
@@ -177,7 +193,7 @@ defmodule Pleroma.Web.NodeInfoTest do
     simple_config = %{"reject" => [{"example.com", ""}, {"other.site", ""}]}
     clear_config(:mrf_simple, simple_config)
 
-    expected_config = %{"reject" => [["example.com", ""]]}
+    expected_config = %{"reject" => [%{"instance" => "example.com", "reason" => ""}]}
 
     response =
       conn
