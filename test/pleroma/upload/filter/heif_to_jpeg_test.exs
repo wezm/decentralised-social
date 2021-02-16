9e0e2fb6b44f37f0ca484e40208edd46cb0f3430
Mark Felder <feld@feld.me>
2021-02-16 09:32:03 -0600

    Fix test name, credo

--- test/pleroma/upload/filter/heif_to_jpeg_test.exs
+++ test/pleroma/upload/filter/heif_to_jpeg_test.exs
@@ -2,7 +2,7 @@
 # Copyright © 2017-2021 Pleroma Authors <https://pleroma.social/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
-defmodule Pleroma.Upload.Filter.HeicToJpegTest do
+defmodule Pleroma.Upload.Filter.HeifToJpegTest do
   use Pleroma.DataCase, async: true
   alias Pleroma.Upload.Filter
 
