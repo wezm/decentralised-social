2449be0cebe4179b00c1bfaf909ee94fbe9b0180
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2021-06-03 19:03:41 +0200

    mix format on OTP 24 + Elixir 1.12.1

--- lib/pleroma/web/activity_pub/mrf.ex
+++ lib/pleroma/web/activity_pub/mrf.ex
@@ -161,9 +161,7 @@ defmodule Pleroma.Web.ActivityPub.MRF do
           [description | acc]
         else
           Logger.warn(
-            "#{policy} config description doesn't have one or all required keys #{
-              inspect(@required_description_keys)
-            }"
+            "#{policy} config description doesn't have one or all required keys #{inspect(@required_description_keys)}"
           )
 
           acc
