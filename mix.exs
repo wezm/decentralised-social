d76fb1e6b438008e9ad89a69b45dc55561030bf1
Mark Felder <feld@feld.me>
2022-11-09 13:45:57 -0500

    Replace ImageMagick with Vips for Media Preview Proxy

--- mix.exs
+++ mix.exs
@@ -198,6 +198,7 @@ defmodule Pleroma.Mixfile do
       {:open_api_spex, "~> 3.10"},
       {:phoenix_live_dashboard, "~> 0.6.2"},
       {:ecto_psql_extras, "~> 0.6"},
+      {:vix, "~> 0.14.0"},
 
       # indirect dependency version override
       {:plug, "~> 1.10.4", override: true},
